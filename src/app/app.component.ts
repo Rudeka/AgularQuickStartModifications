2c5efd263a6e3ce7862e5156886d08a02b973dc3
Ihor Rudenko <irudenko@softserveinc.com>
2017-03-02 20:57:57 +0200

    extract to separate files models and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,24 @@
 import { Component } from '@angular/core';
+import { Customer } from './model';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <h1>{{name}}</h1>
+  templateUrl: `
   
-  <p><i>{{name}} is at {{street}} in {{city}} in the {{region}} region</i></p>
-  <br/>
-
-  <fieldset>
-    <label>Name:</label><br/>
-    <input [(ngModel)]="name"><br/>
-  </fieldset>
-
-  <label><input type="checkbox" [(ngModel)]="hideAddress">Hide Address</label>
-  <div [hidden] = "hideAddress">
-    <fieldset>
-      <label>Street: <input [(ngModel)]="street"></label>
-    </fieldset>
-    <fieldset>
-      <label>City: <input [(ngModel)]="city"></label>
-    </fieldset>
-    <fieldset>
-      <label>Region:</label>
-      <select [(ngModel)] = "region">
-        <option>East</option>
-        <option>North</option>
-        <option>South</option>
-        <option>West</option>
-      </select>
-    </fieldset>
-  </div>
 `
-,
+  ,
 })
 export class AppComponent {
-  name = 'Alex Smith';
-  street = 'Myra';
-  city = 'Obukhiv';
-  region = 'North';
+  customer: Customer = {
+    id: 1,
+    name: 'Alex Smith',
+    address: {
+      city: 'Anytown',
+      street: '123 main street',
+      state: 'California',
+      region: 'east',
+    }
+  };
+
   hideAddress = false;
 }
